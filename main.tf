50dd468ec5bb316b35200efaad0c31029c3ca607
Oleh-Khomenko <olehhomenko12@gmail.com>
2024-05-28 22:45:55 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "my-instance" {
-  ami           = "ami-0b6e9ccdaffb5503f	"
+  ami           = "ami-0b6e9ccdaffb5503f"
   instance_type = "t3.micro"
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  vpc_security_group_ids = ["sg-0b5bfb0241e132fca"]
   key_name      = var.key_name
 
   user_data = <<-EOF
